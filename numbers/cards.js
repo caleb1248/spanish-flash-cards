afbcbb67f7d2ecb7082affa1a4c7c4c6f562c331
caleb1248 <caleb@tremontschool.org>
2022-09-22 21:39:19 +0000

    Add numbers card deck

---
+++ numbers/cards.js
@@ -0,0 +1,19 @@
+const cards = [
+  ["1", "uno"],
+  ["2", "dos"],
+  ["3", "tres"],
+  ["4", "cuatro"],
+  ["5", "cinco"],
+  ["6", "sies"],
+  ["7", "siete"],
+  ["8", "ocho"],
+  ["9", "mueve"],
+  ["10", "dies"],
+  ["11", "once"],
+  ["12", "doce"],
+  ["13", "trece"],
+  ["14", "catorce"],
+  ["15", "quince"],
+  ["20", "veinte"],
+  ["30", "treinta"]
+]
